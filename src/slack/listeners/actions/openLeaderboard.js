0e6f658995d5be7f35a7496373f707d97bf2b8d7
faithfulnesss <torfik1med@gmail.com>
2024-01-09 15:57:41 +0000

    Implement logic for leaderboard interaction

---
+++ src/slack/listeners/actions/openLeaderboard.js
@@ -0,0 +1,52 @@
+const leaderboardView = require("../../views/leaderboardView");
+const employeeRepository = require("../../../database/repositories/employeeRepository");
+
+const PAGE_SIZE = 12;
+
+module.exports = (app) => {
+  app.action("open_leaderboard", async ({ ack, body, client }) => {
+    await ack();
+
+    const employees = await employeeRepository.getEmployees();
+
+    try {
+      await client.views.open({
+        trigger_id: body.trigger_id,
+        view: leaderboardView(employees, 1, PAGE_SIZE),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+
+    app.action(
+      "leaderboard_previous",
+      async ({ ack, body, client, action }) => {
+        await ack();
+        try {
+          const currentPage = parseInt(action.value);
+
+          await client.views.update({
+            view_id: body.view.id,
+            view: leaderboardView(employees, currentPage, PAGE_SIZE),
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    );
+
+    app.action("leaderboard_next", async ({ ack, body, client, action }) => {
+      await ack();
+      try {
+        const currentPage = parseInt(action.value);
+
+        await client.views.update({
+          view_id: body.view.id,
+          view: leaderboardView(employees, currentPage, PAGE_SIZE),
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  });
+};
